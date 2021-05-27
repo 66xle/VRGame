440fed3d1d6dcb19831685dae4a07106f7aedb24
Samuel <robbo_rules1@live.com.au>
2021-05-27 15:32:08 +1000

    the project

---
+++ Vr/Assets/Scripts/PhysicsPusher.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsPusher : MonoBehaviour
+{
+    public Transform pointer;
+
+    LineRenderer lineRenderer;
+
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        Ray ray = new Ray(pointer.position, pointer.forward);
+        RaycastHit hit;
+
+        if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            lineRenderer.positionCount = 2;
+            lineRenderer.SetPosition(0, ray.origin);
+            lineRenderer.SetPosition(1, ray.origin + 100 * ray.direction);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Rigidbody body = hit.collider.GetComponent<Rigidbody>();
+                if (body)
+                {
+                    body.AddForce(10000.0f * ray.direction);
+                }
+            }
+        }
+        else
+        {
+            lineRenderer.positionCount = 0;
+        }
+    }
+}
