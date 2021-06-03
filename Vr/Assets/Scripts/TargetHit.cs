f3946d610f162a6c1c385b8927b2a3272394a5fb
Samuel Le <samuel.le4324@gmail.com>
2021-06-03 16:34:52 +1000

    Target invis when hit
    Disabled mesh collidier, added box collider to cube

--- Vr/Assets/Scripts/TargetHit.cs
+++ Vr/Assets/Scripts/TargetHit.cs
@@ -4,13 +4,44 @@ using UnityEngine;
 
 public class TargetHit : MonoBehaviour
 {
-    public bool isTargetActive;
+    bool isTargetActive = true;
+    float maxDuration;
+    float currentDuration;
+
 
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Bullet")
         {
-            Destroy(transform.gameObject);
+            GameObject go = transform.parent.gameObject;
+            go.GetComponentsInChildren<MeshRenderer>()[0].enabled = false;
+            go.GetComponentsInChildren<MeshRenderer>()[1].enabled = false;
+
+
+            go.GetComponentsInChildren<MeshCollider>()[0].enabled = false;
+
+            isTargetActive = false;
+
+            currentDuration = 0.0f;
+            maxDuration = Time.deltaTime + 2.0f;
+        }
+    }
+
+    void Update()
+    {
+        if (!isTargetActive && currentDuration > maxDuration)
+        {
+            GameObject go = transform.parent.gameObject;
+            go.GetComponentsInChildren<MeshRenderer>()[0].enabled = true;
+            go.GetComponentsInChildren<MeshRenderer>()[1].enabled = true;
+
+            go.GetComponentsInChildren<MeshCollider>()[0].enabled = true;
+
+            isTargetActive = true;
+        }
+        else
+        {
+            currentDuration += Time.deltaTime;
         }
     }
 }
